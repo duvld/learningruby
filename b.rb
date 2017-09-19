6b29f1aad23b1587cfb1b8bd800e9053ac0cd668
duvld <ollejna@gmail.com>
2017-09-19 16:20:48 -0400

    use of classes

--- b.rb
+++ b.rb
@@ -1,7 +1,30 @@
 #!/usr/bin/ruby -w
 
-class Customer
-	@@no_of_customer = 0;
+#Class creation
 
-	def display_details
-		puts "YOU ARE CUSTOMER NUMBER: #@no_of_customer"
+class Customer #how to make a class and use it
+	@@no_of_customer = 0; #this is a class variable i.e. static
+
+	def initialize(first, last, id) #same as a java constructor
+		@cust_first = first #the one @ variable is an instance variable
+		@cust_last = last #i.e. a varible for the specific instance
+		@cust_id = id
+	end
+
+	def display_details() #a local method for this class
+		puts "YOU ARE CUSTOMER NUMBER: #@cust_id" 
+		#how to put variables in a puts statement
+		puts "YOUR NAME IS #@cust_first #@cust_last"
+	end
+
+end
+
+#Class initalization
+
+cust1 = Customer.new("ian", "dumbo", "69")
+cust2 = Customer.new("tipeeshan", "dumbass", "420")
+
+#Use of instance methods
+
+cust1.display_details()
+cust2.display_details()
